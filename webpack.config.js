8b9e6cac17cf4aa75868edcdaa5259f9708c308f
lovemoganna <15733207536@163.com>
2018-11-02 11:17:04 +0800

    webpack.config.js is more level config and webpack.part.js is less level config

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,32 @@
-/**
- * @fileOverview
- * @name webpack.config.js
- * @author luoyupiaoshang
- * @license MIT
- */
-
-const path = require("path");
-const webpack = require("webpack");
+const merge = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-///////////////////////////////////////////////////////////////////////////////
-//                     add html-webpack-plugin in webpack                    //
-/////////////////////////////////////////////////////////////////////////////// 
 
-module.exports = {
-    devServer: {
-        watchOptions: {
-            // Delay the rebuild after the first change
-            aggregateTimeout: 300,
+const parts = require("./webpack.parts");
 
-            // Poll using interval (in ms, accepts boolean too)
-            poll: 1000,
-        },
-        // Display only errors to reduce the amount of output.
-        stats: "errors-only",
-
-        // Parse host and port from env to allow customization.
-        //
-        // If you use Docker, Vagrant or Cloud9, set
-        // host: options.host || "0.0.0.0";
-        //
-        // 0.0.0.0 is available to all network devices
-        // unlike default `localhost`.
-        host: "0.0.0.0",
-        port: process.env.PORT, // Defaults to 8080
-        open: false, // Open the page in browser
-        overlay: true,
+const commonConfig = merge([
+    {
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: "Webpack demo",
+            }),
+        ],
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "Webpack demo",
-        }),
-        // Ignore node_modules so CPU usage with poll
-        // watching drops significantly.
-        new webpack.WatchIgnorePlugin([
-            path.join(__dirname, "node_modules")
-        ]),
-    ],
+]);
+
+const productionConfig = merge([]);
+
+const developmentConfig = merge([
+    parts.devServer({
+        // Customize host/port here if needed
+        host:"0.0.0.0 0",
+        port: process.env.PORT,
+    }),
+]);
+ 
+module.exports = mode => {
+    if (mode === "production") {
+        return merge(commonConfig, productionConfig, { mode });
+    }
+
+    return merge(commonConfig, developmentConfig, { mode });
 };
---
+++ webpack.parts.js
@@ -0,0 +1,10 @@
+exports.devServer = ({ host, port } = {}) => ({
+    devServer: {
+        stats: "errors-only",
+        host, // Defaults to `localhost`
+        port, // Defaults to 8080
+        open: true, 
+        overlay: true,
+    },
+});
+
