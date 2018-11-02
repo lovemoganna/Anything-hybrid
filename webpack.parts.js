1600a95b348af82b75b96ef5968fd979484c66ef
lovemoganna <15733207536@163.com>
2018-11-02 16:18:01 +0800

    separating css in the js. make true css show style

--- webpack.parts.js
+++ webpack.parts.js
@@ -1,10 +1,78 @@
-exports.devServer = ({ host, port } = {}) => ({
+// use separating css plugin 
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+exports.devServer = ({
+    host,
+    port
+} = {}) => ({
     devServer: {
         stats: "errors-only",
         host, // Defaults to `localhost`
         port, // Defaults to 8080
-        open: true, 
+        open: true,
         overlay: true,
     },
 });
 
+// parser css assert 
+// add sass ,need install node-sass 
+exports.loadCSS = ({
+    include,
+    exclude
+} = {}) => ({
+    module: {
+        rules: [{
+            test: /\.css$/,
+            include,
+            exclude,
+            use: ["style-loader",
+                {
+                    // parse sass file
+                    loader: "css-loader",
+                    options: {
+                        importLoaders: 2,
+                    },
+                },
+                "sass-loader",
+                {
+                    loader: "postcss-loader",
+                    options: {
+                        plugins: () => ([
+                            require("precss"),
+                            require("postcss-cssnext"),
+                        ]),
+                    },
+                },
+            ],
+        }, ],
+    },
+});
+
+
+// setting up MCEP
+
+exports.extractCSS = ({
+    include,
+    exclude,
+    use = []
+}) => {
+    // Output extracted CSS to a file
+    const plugin = new MiniCssExtractPlugin({
+        filename: "[name].css",
+    });
+
+    return {
+        module: {
+            rules: [{
+                test: /\.css$/,
+                include,
+                exclude,
+
+                use: [
+                    MiniCssExtractPlugin.loader,
+                ].concat(use),
+            }, ],
+        },
+        plugins: [plugin],
+    };
+};
