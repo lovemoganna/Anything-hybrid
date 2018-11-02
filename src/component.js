39b0a74ec7441d60495747b2a5a9b6f4e2012565
lovemoganna <15733207536@163.com>
2018-11-02 17:36:00 +0800

    test and generate file

--- src/component.js
+++ src/component.js
@@ -1,6 +1,7 @@
 
-export default (text = "hello webpack") => {
+export default (text = "Hello world!") => {
     const element = document.createElement("div");
+    element.className = "pure-button";
     element.innerHTML = text ;
     return element;
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "purecss";
 import "./main.css";
 import component from "./component";
 
