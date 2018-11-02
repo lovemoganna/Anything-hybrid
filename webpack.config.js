3a67c8d68fd95aa9c8bb8869a9e82e9bb6b7d973
lovemoganna <15733207536@163.com>
2018-11-02 16:19:36 +0800

    add entry point .and handle css file in dev environment.

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,54 @@
+const glob = require("glob");
 const merge = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const parts = require("./webpack.parts");
 
-const commonConfig = merge([
-    {
+
+// common config , there no config css
+const commonConfig = merge([{
         plugins: [
             new HtmlWebpackPlugin({
                 title: "Webpack demo",
             }),
         ],
+        entry: {
+            //this is a entry point,maybe have css sort problem.you should strict fllow css import rule in sequence
+            // if you don't use MCEP ,you could install css-entry-webpack-plugina. The plugin can extract css bundle from entry without MECP.
+            style: glob.sync("./src/**/*.css"),
+        },
     },
+    //  parts.loadCSS(),
 ]);
 
-const productionConfig = merge([]);
+//const productionConfig = merge([]);
 
+// dev config 
 const developmentConfig = merge([
     parts.devServer({
         // Customize host/port here if needed
-        host:"0.0.0.0 0",
+        host: "0.0.0.0",
         port: process.env.PORT,
     }),
+    parts.loadCSS(),
 ]);
- 
+
+
+// pro config 
+const productionConfig = merge([
+    parts.extractCSS({
+        use: "css-loader",
+    }),
+]);
+
 module.exports = mode => {
     if (mode === "production") {
-        return merge(commonConfig, productionConfig, { mode });
+        return merge(commonConfig, productionConfig, {
+            mode
+        });
     }
 
-    return merge(commonConfig, developmentConfig, { mode });
+    return merge(commonConfig, developmentConfig, {
+        mode
+    });
 };
