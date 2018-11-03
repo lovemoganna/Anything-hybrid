4e668f0880539ebaec4795d290e12a381f0ed9bc
lovemoganna <15733207536@163.com>
2018-11-03 12:29:22 +0800

    add case-sensitive-path-plugin responsive

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require("path");
 const glob = require("glob");
 const merge = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
 const parts = require("./webpack.parts");
 
 // define work directory 
@@ -12,16 +14,41 @@ const PATHS = {
 
 // common config , there no config css
 const commonConfig = merge([{
+        devtool: 'eval-source-map',
         plugins: [
             new HtmlWebpackPlugin({
                 title: "Webpack demo",
             }),
+            new CaseSensitivePathsPlugin(),
+            new FriendlyErrorsPlugin({
+                compilationSuccessInfo: {
+                    messages: [`You application is running! `],
+
+                    notes: ['Some additionnal notes to be displayed unpon successful compilation']
+                },
+                onErrors: function(severity, errors) {
+                    // You can listen to errors transformed and prioritized by the plugin
+                    // severity can be 'error' or 'warning'
+                },
+                // should the console be cleared between each compilation?
+                // default is true
+                clearConsole: true,
+
+                // add formatters and transformers (see below)
+                additionalFormatters: [],
+                additionalTransformers: []
+            })
         ],
         entry: {
             //this is a entry point,maybe have css sort problem.you should strict fllow css import rule in sequence
             // if you don't use MCEP ,you could install css-entry-webpack-plugina. The plugin can extract css bundle from entry without MECP.
-            style: glob.sync("./src/**/*.css"),
+            // style: glob.sync("./src/**/*.css"),  
+            main: __dirname + "/src/index.js",
         },
+        output: {
+            path: __dirname + "/public",
+            filename: "bundle.js"
+        }
     },
     //  parts.loadCSS(),
 ]);
@@ -45,7 +72,9 @@ const productionConfig = merge([
         use: "css-loader",
     }),
     parts.purifyCSS({
-        paths: glob.sync(`${PATHS.app}/**/*.js`, { nodir: true }),
+        paths: glob.sync(`${PATHS.app}/**/*.js`, {
+            nodir: true
+        }),
     }),
 ]);
 
